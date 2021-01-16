fc6d726f93ec44731aaca90f8a05eb3a989c6c7d
barry <50138085+AbdoulRahimBarry@users.noreply.github.com>
2021-01-16 18:05:00 +0100

    Update index.php

--- website/index.php
+++ website/index.php
@@ -8,7 +8,7 @@
         <h1>Student Checking rApp</h1>
         <ul>
             <form action="" method="POST">
-             <label>Enter student name:</label><br />
+            <label>Enter student name:</label><br />
             <input type="text" name="" placeholder="Student Name" required/>
             <br /><br />
             <button type="submit" name="submit">List Student</button>
